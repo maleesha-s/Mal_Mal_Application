323658e8fcee4d4d04ea5a5d74ff28b279d16948
Maleesha Suraj <maleesha@ecyber.com>
2022-03-19 13:21:29 +0530

    signup component added

--- frontend_mobile/lib/main.dart
+++ frontend_mobile/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:frontend_mobile/screens/Login/login.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend_mobile/screens/SignUp/signup.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Login(),
+      home: const Signup(),
     );
   }
 }
---
+++ frontend_mobile/lib/screens/SignUp/components/body.dart
@@ -0,0 +1,93 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+//user class
+class User{
+  final String userName;
+  final String firstName;
+  final String lastName;
+  final String password;
+
+  const User({required this.userName, required this.firstName, required  this.lastName, required this.password});
+
+  factory User.fromJson(Map<String, dynamic> json){
+    return User(
+      userName: json['userName'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      password: json['password']
+    );
+  }
+}
+
+//http request
+Future<User> addNewUser(String userName, String firstName, String lastName, String password) async {
+  final response = await http.post(
+    Uri.parse('http://localhost:8070/users/addUser'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, String>{
+      'userName': userName,
+      'firstName':firstName,
+      'lastName':lastName,
+      'password':password
+    }),
+  );
+  if (response.statusCode == 201) {
+    return User.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to create album.');
+  }
+}
+class Body extends StatefulWidget {
+  const Body({ Key? key }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _BodyState();
+  }
+  
+}
+
+class _BodyState extends State<Body> {
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController _controllerFirstName = TextEditingController();
+    final TextEditingController _controllerLastName = TextEditingController();
+    final TextEditingController _controllerUserName = TextEditingController();
+    final TextEditingController _controllerPassword = TextEditingController();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        TextField(
+          controller: _controllerFirstName,
+          decoration: const InputDecoration(hintText: 'First Name : '),
+        ),
+        TextField(
+          controller: _controllerLastName,
+          decoration: const InputDecoration(hintText: 'Last Name : '),
+        ),
+        TextField(
+          controller: _controllerUserName,
+          decoration: const InputDecoration(hintText: 'User Name : '),
+        ),
+        TextField(
+          controller: _controllerPassword,
+          decoration: const InputDecoration(hintText: 'Password : '),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              addNewUser(_controllerUserName.text,_controllerFirstName.text, _controllerLastName.text, _controllerPassword.text);
+             });
+          },
+          child: const Text('Create Data'),
+        ),
+      ],
+    );
+  }
+}
